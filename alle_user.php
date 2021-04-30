1dddf0878a982d5b2becb764e010bb7e52cce722
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-04-30 18:02:08 +0200

    V.1.0.33

--- alle_user.php
+++ alle_user.php
@@ -4,7 +4,7 @@ include 'database_login.php';
 <html lang="de">
 	<head><title>socnet - Das soziale Netzwerk</title></head>
 	<body onload="getCookie()">
-    <?php include "./header.html" ?>
+    <!--<?php include "./header.html" ?>
 		<br>
 		<table border="1">
 		<tr>
@@ -32,7 +32,7 @@ include 'database_login.php';
 			}
 			?>
 
-		</table>
+		</table>-->
 
 	<!--<script type="text/javascript" src="cookie.js"></script>-->
 
@@ -45,7 +45,38 @@ include 'database_login.php';
                 var c = ca[i].trim();
                 if ((c.indexOf(name)) == 0) {
                     console.log("Anmeldung erfolgreich.");
-                    document.getElementById('credentials').innerHTML = 'Angemeldet'
+                    document.write(
+                        <?php include "./header.html" ?>
+                        <br>
+                            <table border="1">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Vorname</th>
+                                    <th>Nutzername</th>
+                                    <th>E-Mail</th>
+                                    <th>Passwort</th>
+                                </tr>
+
+                                <?php
+                                $select = "select p_id, name, vorname, username, passwort, email from person";
+                                $result = mysql_query($select);
+                                while ($daten = mysql_fetch_array($result))
+                                {
+                                    echo "<tr>";
+                                    echo "<td>".$daten['p_id']."</td>";
+                                    echo "<td>".$daten['name']."</td>";
+                                    echo "<td>".$daten['vorname']."</td>";
+                                    echo "<td>".$daten['username']."</td>";
+                                    echo "<td>".$daten['email']."</td>";
+                                    echo "<td>".$daten['passwort']."</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+
+                            </table>
+
+                    );
                     return c.substr(name.length);
                 }
 
