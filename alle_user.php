d3bb947ba827375e074d48e0bb55065720593509
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-05-07 13:43:28 +0200

    V.1.0.58

--- alle_user.php
+++ alle_user.php
@@ -18,14 +18,14 @@ include 'database_login.php';
         }
     </script>
 
-    <script type="text/javascript">
+    <!--<script type="text/javascript">
         function getSecureCookie() {
             const value_or_null = (document.cookie.match(/^(?:.*;)?\s*user\s*=\s*([^;]+)(?:.*)?$/) || [, null])[1];
             if (value_or_null == null) {
                 window.location.href = 'index.php';
             }
         }
-    </script>
+    </script>-->
 
     <script>
         getSecureCookie();
