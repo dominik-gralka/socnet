97b9161421f932e529728f9d7e40bd895950ed95
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-05-07 13:55:20 +0200

    V.1.0.60

--- user_hinzufuegen.php
+++ user_hinzufuegen.php
@@ -8,7 +8,7 @@ $fusername = $_POST['username'];
 $fpasswort = $_POST['passwort'];
 $femail = $_POST['email'];
 
-$insert = "insert into person(name, vorname,username,passwort,email) values ('$fname', '$fvorname', '$fusername', password('$fpasswort'),'$femail')";
+$insert = "insert into person(name, vorname,username,passwort,email) values ('$fname', '$fvorname', '$fusername', '$fpasswort','$femail')";
 // echo "F&uuml;hre den Befehl ".$insert." aus.";
 $result = mysql_query($insert);
 
